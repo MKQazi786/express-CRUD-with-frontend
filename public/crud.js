bd0137789c18923ec3455f7f4dfa4566bcbc0e10
Muhammad Khubaib <muhammadkhubaib492@gmail.com>
2023-10-09 01:44:09 +0500

    i have added updated form in my post but it doesnt work

--- public/crud.js
+++ public/crud.js
@@ -30,11 +30,11 @@ window.getAllPost = (event) => {
             let postsHtml = ``
             response.data.map((eachPost) => {
                 postsHtml +=
-                    `<div class="post-card">
+                    `<div id="card-${eachPost.id}" class="post-card">
                     <h3> ${eachPost.title} </h3> 
                     <p> ${eachPost.text} </p>
                     <button onclick="delPost('${eachPost.id}')"> Delete </button>
-                    <button> Edit </button>
+                    <button onclick="editPost('${eachPost.id}','${eachPost.title}','${eachPost.text}')"> Edit </button>
                 </div> 
                 <br>`
             })
@@ -62,5 +62,39 @@ window.getAllPost = (event) => {
                 console.log(error.data);
                 document.getElementById("result").innerHTML = "error in post"
             })
-    } 
+    }
+
+    window.editPost = (postId, title, text) => {
+        console.log("PostId :", postId)
+
+        document.getElementById(`card-${postId}`).innerHTML += 
+        `<form onsubmit="savePost('${postId}')" >
+        <br/>
+          title: <input type="text" value="${title}" id="title-${postId}" />
+          <br/>
+          text: <input type="text" value="${text}" id="text-${postId}" />
+          <br/>
+          <button onclick="savePost('${postId}')" >Save</button>
+        </form>`
+
+    }
+
+    window.savePost = (postId) => {
+        const updatedTitle = document.getElementById(`title-${postId}`).value
+        const updatedText = document.getElementById(`text-${postId}`).value
+
+        axios.put(`/api/v1/post/${postId}`, {
+            title: updatedTitle,
+            text: updatedText
+        })
+            .then(function (response) {
+                console.log(response.data);
+                getAllPost()
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error.data);
+                document.getElementById("result").innerHTML = "error in post"
+            })
+}
 }
